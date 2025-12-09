8bf42f5d29c248a4748f8aba8624607d669b6d26
Jayden <JaydenRoberts269@gmail.com>
2025-12-09 03:41:10 +0000

    firstFile

---
+++ aprogram.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    printf("This is a line of text being printed to the screen!!!!\n");
+    if (argc > 1){
+        printf("there is more than one argument");
+    }
+    else{
+        printf("the other one");
+    }
+    int value = 15;
+    int newValue = value | 12
+    printf("%d", newValue)
+}
